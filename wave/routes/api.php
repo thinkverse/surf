e10aa55104afffc39c1265e825171ae9b4ce02c3
thinkverse <hallberg.kim@gmail.com>
2022-05-14 02:36:08 +0200

    Update API routes to use FQCN

--- wave/routes/api.php
+++ wave/routes/api.php
@@ -1,24 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Wave\Http\Controllers\API\ApiController;
+use Wave\Http\Controllers\API\AuthController;
 
-Route::post('login', '\Wave\Http\Controllers\API\AuthController@login');
-Route::post('register', '\Wave\Http\Controllers\API\AuthController@register');
-Route::post('logout', '\Wave\Http\Controllers\API\AuthController@logout');
-Route::post('refresh', '\Wave\Http\Controllers\API\AuthController@refresh');
-Route::post('token', '\Wave\Http\Controllers\API\AuthController@token');
+// Auth
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('logout', [AuthController::class, 'logout']);
+Route::post('refresh', [AuthController::class, 'refresh']);
+Route::post('token', [AuthController::class, 'token']);
 
-// BROWSE
-Route::get('/{datatype}', '\Wave\Http\Controllers\API\ApiController@browse');
-
-// READ
-Route::get('/{datatype}/{id}', '\Wave\Http\Controllers\API\ApiController@read');
-
-// EDIT
-Route::put('/{datatype}/{id}', '\Wave\Http\Controllers\API\ApiController@edit');
-
-// ADD
-Route::post('/{datatype}', '\Wave\Http\Controllers\API\ApiController@add');
-
-// DELETE
-Route::delete('/{datatype}/{id}', '\Wave\Http\Controllers\API\ApiController@delete');
+// BREAD
+Route::get('/{datatype}', [ApiController::class, 'browse']);
+Route::get('/{datatype}/{id}', [ApiController::class, 'read']);
+Route::put('/{datatype}/{id}', [ApiController::class, 'edit']);
+Route::post('/{datatype}', [ApiController::class, 'add']);
+Route::delete('/{datatype}/{id}', [ApiController::class, 'delete']);
