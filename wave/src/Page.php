76f1f1d3e3b1a13f49d483688db52105221e6c40
thinkverse <hallberg.kim@gmail.com>
2022-05-19 12:43:36 +0200

    Add privacy and terms pages
    Closes #3

--- wave/src/Page.php
+++ wave/src/Page.php
@@ -2,16 +2,37 @@
 
 namespace Wave;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Support\Str;
 use TCG\Voyager\Facades\Voyager;
+use TCG\Voyager\Models\Page as Model;
 
 class Page extends Model
 {
-	public function link(){
-    	return url('p/' . $this->slug);
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'author_id',
+        'title',
+        'excerpt',
+        'body',
+        'image',
+        'slug',
+        'meta_description',
+        'meta_title',
+        'status',
+    ];
+
+    public function link()
+    {
+        return url('p/' . $this->slug);
     }
 
-    public function image(){
-    	return Voyager::image($this->image);
+    public function image()
+    {
+        return Voyager::image($this->image);
     }
 }
