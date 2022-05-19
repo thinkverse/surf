07c2a2716920f0e791360acad2170bd84f9160e6
thinkverse <hallberg.kim@gmail.com>
2022-05-19 21:51:52 +0200

    Publish well-known config

---
+++ config/well-known.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+return [
+    /**
+     * Redirects - Redirect 302 routes.
+     */
+    'redirects' => [
+        'change-password' => '/settings/security',
+    ],
+
+    /**
+     * Static - GET routes.
+     */
+    'static' => [
+        // 'security.txt' => fn () => response('Hello world.'),
+    ],
+];
