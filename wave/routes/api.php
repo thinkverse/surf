4ae6fe42cea3cf1212273e13ce96d4dfae9b3e25
thinkverse <hallberg.kim@gmail.com>
2022-06-20 16:30:14 +0200

    Rewrite api routes

--- wave/routes/api.php
+++ wave/routes/api.php
@@ -1,19 +1,26 @@
 <?php
 
+use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
 use Wave\Http\Controllers\API\ApiController;
 use Wave\Http\Controllers\API\AuthController;
 
 // Auth
-Route::post('login', [AuthController::class, 'login']);
-Route::post('register', [AuthController::class, 'register']);
-Route::post('logout', [AuthController::class, 'logout']);
-Route::post('refresh', [AuthController::class, 'refresh']);
-Route::post('token', [AuthController::class, 'token']);
+Route::controller(AuthController::class)
+    ->group(function (Router $router) {
+        $router->post('/login', 'login');
+        $router->post('/register', 'register');
+        $router->post('/logout', 'logout');
+        $router->post('/refresh', 'refresh');
+        $router->post('/token', 'token');
+    });
 
 // BREAD
-Route::get('/{datatype}', [ApiController::class, 'browse']);
-Route::get('/{datatype}/{id}', [ApiController::class, 'read']);
-Route::put('/{datatype}/{id}', [ApiController::class, 'edit']);
-Route::post('/{datatype}', [ApiController::class, 'add']);
-Route::delete('/{datatype}/{id}', [ApiController::class, 'delete']);
+Route::controller(ApiController::class)
+    ->group(function (Router $router) {
+        $router->get('/{datatype}', 'browse');
+        $router->get('/{datatype}/{id}', 'read');
+        $router->post('/{datatype}/{id}', 'edit');
+        $router->post('/{datatype}', 'add');
+        $router->delete('/{datatype}/{id}', 'delete');
+    });
